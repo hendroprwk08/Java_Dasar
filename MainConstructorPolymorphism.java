75ec10731763cfa7b05dff8cb68811edf07c0808
hendroprwk08 <hendroprwk08@gmail.com>
2019-11-19 10:46:47 +0700

    ok

--- MainConstructorPolymorphism.java
+++ MainConstructorPolymorphism.java
@@ -1,45 +1,43 @@
-/*
- * Pertemuan 3a: Construct dan Overloading
- */
+public class MainConstructorPolymorphism{
 
-public class Karyawan
-{
-    private int noKar;
-    private String nama, alamat;
-    
-    public Karyawan(int k, String n, String a)
+    public static void main(String[]args)
     {
+        Karyawan Kar1 = new Karyawan(171234,"Andi","Jln Tamrin");        
+        //menampilkan data
+        System.out.println ("Nomor Karyawan : "+ Kar1.noKar);
+        System.out.println ("Nama Karyawan : "+ Kar1.nama);
+        System.out.println ("Alamat karyawan : "+ Kar1.alamat);
+        
+        Karyawan Kar2 = new Karyawan();
+        System.out.println ("Nomor Karyawan : "+ Kar2.noKar);
+        System.out.println ("Nama Karyawan : "+ Kar2.nama);
+        System.out.println ("Alamat karyawan : "+ Kar2.alamat);
+        
+        Karyawan Kar3 = new Karyawan("Jokowi");
+        System.out.println ("Nomor Karyawan : "+ Kar3.noKar);
+        System.out.println ("Nama Karyawan : "+ Kar3.nama);
+        System.out.println ("Alamat karyawan : "+ Kar3.alamat);
+    }
+}
+
+class Karyawan
+{
+    int noKar;
+    String nama, alamat;
+    
+    Karyawan(int k, String n, String a)
+    { 
         this.noKar= k;
         this.nama = n;
         this.alamat = a; 
     }
     
     //konstruktor tanpa parameter
-    public Karyawan(){}
+    Karyawan(){}
     
     //konstruktor dengan satu parameter
-    public Karyawan(String n)
+    Karyawan(String n)
     {
         this.nama = n;
-    }
-    
-    public static void main(String[]args)
-    {
-        Karyawan Kar1 = new Karyawan(171234,"Andi","Jln Tamrin");
-        Karyawan Kar2 = new Karyawan();
-        Karyawan Kar3 = new Karyawan("Jokowi");
-        
-        //menampilkan data
-        System.out.println ("Nomor Karyawan : "+ Kar1.noKar);
-        System.out.println ("Nama Karyawan : "+ Kar1.nama);
-        System.out.println ("Alamat karyawan : "+ Kar1.alamat);
-        
-        System.out.println ("Nomor Karyawan : "+ Kar2.noKar);
-        System.out.println ("Nama Karyawan : "+ Kar2.nama);
-        System.out.println ("Alamat karyawan : "+ Kar2.alamat);
-        
-        System.out.println ("Nomor Karyawan : "+ Kar3.noKar);
-        System.out.println ("Nama Karyawan : "+ Kar3.nama);
-        System.out.println ("Alamat karyawan : "+ Kar3.alamat);
     }
 }
--- MainPolymorphism.java
+++ MainPolymorphism.java
@@ -1,4 +1,4 @@
-
+ 
 class CetakDataTipe {
 
     // membuat method dengan nama sama tapi berbeda tipe data
@@ -19,22 +19,21 @@ class CetakDataTipe {
     }
 }
 
-
 public class MainPolymorphism {
 
     public static void main(String[] args) {
         CetakDataTipe data = new CetakDataTipe();
         
-         System.out.print("Cetak Data Tipe String : ");
+        System.out.print("Cetak Data Tipe String : ");
         data.CetakData("Sumatra");
         
         System.out.print("Cetak Data Tipe Integer : ");
         data.CetakData(1989);
         
         System.out.print("Cetak Data Tipe Double : ");
-        data.CetakData(16.5 / 2);
+        data.CetakData(16.5 / 2); 
         
         System.out.print("Cetak Data Tipe Char : ");
-        data.CetakData('N');
+        data.CetakData("N");
     }
 }
